f8d3fff2f07b3dd8ed7f93b377b5ce57f22ed50d
thellost <71958338+thellost@users.noreply.github.com>
2025-03-16 08:32:02 +0900

    delete

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-export { default } from "next-auth/middleware"
-
-export const config = { matcher: ["/dashboard" , "/profile"] }
