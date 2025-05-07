baf5111b3a77267694cc774127b2f4f6bd5f7269
thellost <71958338+thellost@users.noreply.github.com>
2025-05-07 17:08:09 +0700

    chromium

--- app/api/scrape/puppeteer.ts
+++ app/api/scrape/puppeteer.ts
@@ -1,9 +1,11 @@
-import chromium from 'chrome-aws-lambda';
-const browser = await chromium.puppeteer.launch({
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium"
+console.log(await chromium.executablePath)
+const browser = await puppeteer.launch({
   args: chromium.args,
   defaultViewport: chromium.defaultViewport,
   executablePath: await chromium.executablePath,
-  headless: chromium.headless,
 });
 console.log("Puppeteer browser launched successfully.");
+
 export default browser
